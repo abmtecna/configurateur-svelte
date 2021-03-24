6b5570242b47884add39b3e2d6f94817fd323696
Jules Coulon <web@julescoulon.fr>
2021-03-24 17:18:51 +0100

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,46 @@
 <script>
   import { onMount } from "svelte";
-  export let date;
-
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
+  const apiURL = "./data.json";
+  let data = [];
+  onMount(async function () {
+    const response = await fetch(apiURL);
+    data = await response.json();
   });
+
+  let code;
 </script>
 
-<main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
-</main>
+<h1>Configurateur ABM TECNA</h1>
+{#each data as category}
+  <h2>{category.name}</h2>
+  {#each category.content as form}
+    <form>
+      <fieldset>
+        <legend>{form.name}</legend>
+        {#each form.content as label}
+          <div class="columns">
+            <div>
+              {label.name} :
+              <div class="input-list">
+                {#each label.content as input}
+                  <label>
+                    <input type="radio" bind:group={label.name} value={input} />
+                    <span>{input}</span>
+                  </label>
+                {/each}
+              </div>
+            </div>
+            <div>
+              <details>
+                <summary>Aide</summary>
+                <p>Petite description avec images, texte, et schémas...</p>
+              </details>
+            </div>
+          </div>
+        {/each}
+      </fieldset>
+    </form>
+  {/each}
+{/each}
+
+<input value={code} placeholder="Code" />
